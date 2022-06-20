a5328b6204843789e2ac2ddc80a22da903568df4
Sander Jochems <sanderjochems@hotmail.nl>
2022-06-20 11:35:37 +0200

    Create IStore interface

---
+++ Application/Store.fs
@@ -0,0 +1,10 @@
+﻿namespace Pinfold.Store
+
+open Pinfold.Database
+
+type IStore =
+    abstract pinneries: InMemoryDatabase<string, string * string>
+
+    abstract pins: InMemoryDatabase<string, string * decimal * string>
+
+    abstract users: InMemoryDatabase<string, string * string * Option<string>>
