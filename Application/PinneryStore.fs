78ba748e85b2ce35e3d1ede0138517a501d20dda
Sander Jochems <sanderjochems@hotmail.nl>
2022-06-21 16:01:05 +0200

    Create PinneryStore

---
+++ Application/PinneryStore.fs
@@ -0,0 +1,8 @@
+﻿namespace Pinfold.Application.PinneryStore
+
+open Pinfold.Model
+
+type IPinneryStore =
+    abstract all: seq<Pinnery>
+
+    abstract get: string -> Option<Pinnery>
---
+++ DataAccess/PinneryStore.fs
@@ -0,0 +1,27 @@
+﻿module Pinfold.DataAccess.PinneryStore
+
+open Pinfold.Application.PinneryStore
+open Pinfold.Application.PinStore
+open Pinfold.DataAccess.Database
+open Pinfold.DataAccess.Store
+open Pinfold.Model
+
+type PinneryStore(store: Store, pinStore: IPinStore) =
+    let pinneryStore = store.pinneries
+
+    let mapPinnery ((name, location): string * string) : Pinnery =
+        let pins =
+            pinStore.wherePinnery name |> Seq.toList
+
+        { Pinnery.Name = name
+          Location = location
+          Pins = pins }
+
+    interface IPinneryStore with
+        member this.all =
+            InMemoryDatabase.all pinneryStore
+            |> Seq.map mapPinnery
+
+        member this.get(name: string) =
+            InMemoryDatabase.lookup name pinneryStore
+            |> Option.map mapPinnery
