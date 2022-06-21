b00f7a16466dd01a5a355077dd3998253c1a7e1f
Sander Jochems <sanderjochems@hotmail.nl>
2022-06-21 16:01:13 +0200

    Create UserStore

---
+++ Application/UserStore.fs
@@ -0,0 +1,14 @@
+﻿namespace Pinfold.Application.UserStore
+
+open Pinfold.Model
+
+type IUserStore =
+    abstract all: seq<User>
+
+    abstract get: string -> Option<User>
+
+    abstract login: string -> string -> Option<User>
+
+    abstract insert: User -> bool
+
+    abstract update: User -> User -> unit
---
+++ DataAccess/UserStore.fs
@@ -0,0 +1,38 @@
+﻿module Pinfold.DataAccess.UserStore
+
+open Pinfold.Application.UserStore
+open Pinfold.DataAccess.Database
+open Pinfold.DataAccess.Store
+open Pinfold.Model
+
+type UserStore(store: Store) =
+    let userStore = store.users
+
+    let mapUser ((username, password, pinnery): string * string * Option<string>) : User =
+        { User.Username = username
+          Password = password
+          Pinnery = pinnery }
+
+    interface IUserStore with
+        member this.all =
+            InMemoryDatabase.all userStore |> Seq.map mapUser
+
+        member this.get(username: string) =
+            InMemoryDatabase.lookup username userStore
+            |> Option.map mapUser
+
+        member this.login (username: string) (password: string) =
+            InMemoryDatabase.filter (fun (dbUsername, dbPassword, _) -> dbUsername = username && dbPassword = User.hashPassword password) userStore
+            |> Seq.tryHead
+            |> Option.map mapUser
+
+        member this.insert(user: User) =
+            let result =
+                InMemoryDatabase.insert user.Username (user.Username, user.Password, user.Pinnery) userStore
+
+            match result with
+            | Ok _ -> true
+            | Error _ -> false
+
+        member this.update (user: User) (newUser: User) =
+            InMemoryDatabase.update user.Username (newUser.Username, newUser.Password, newUser.Pinnery) userStore
