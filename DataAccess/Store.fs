166803e178694befe9fb6b2650c7be4b23b0454f
Sander Jochems <sanderjochems@hotmail.nl>
2022-06-21 16:01:43 +0200

    Update Store.fs

--- DataAccess/Store.fs
+++ DataAccess/Store.fs
@@ -1,30 +1,29 @@
-module Pinfold.Store
+module Pinfold.DataAccess.Store
 
-open Pinfold.Store
-open Pinfold.Database
+open Pinfold.Model
+open Pinfold.DataAccess.Database
 
 type Store() =
-    interface IStore with
-        member this.pinneries =
-            [ "OggPins", "Lancre"
-              "Pingum", "Genua"
-              "LaBroche", "Quirm"
-              "ASG", "Uberwald" ]
-            |> Seq.map (fun (name, location) -> name, (name, location))
-            |> InMemoryDatabase.ofSeq
+    member val pinneries: InMemoryDatabase<string, string * string> =
+        [ "OggPins", "Lancre"
+          "Pingum", "Genua"
+          "LaBroche", "Quirm"
+          "ASG", "Uberwald" ]
+        |> Seq.map (fun (name, location) -> name, (name, location))
+        |> InMemoryDatabase.ofSeq
 
-        member this.pins =
-            [ "OP1", 100.0m, "OggPins"
-              "OP2", 50m, "OggPins"
-              "PG1", 10m, "Pingum"
-              "LB Deluxe", 5m, "LaBroche"
-              "SuperStifte", 13.37m, "ASG" ]
-            |> Seq.map (fun (name, value, pinnery) -> name, (name, value, pinnery))
-            |> InMemoryDatabase.ofSeq
+    member val pins: InMemoryDatabase<string, string * decimal * string> =
+        [ "OP1", 100.0m, "OggPins"
+          "OP2", 50m, "OggPins"
+          "PG1", 10m, "Pingum"
+          "LB Deluxe", 5m, "LaBroche"
+          "SuperStifte", 13.37m, "ASG" ]
+        |> Seq.map (fun (name, value, pinnery) -> name, (name, value, pinnery))
+        |> InMemoryDatabase.ofSeq
 
-        member this.users =
-            [ "sander", User.hashPassword "Password12", None
-              "jeroen", User.hashPassword "Password34", Some "Pingum"
-              "martijn", User.hashPassword "Password56", Some "LaBroche" ]
-            |> Seq.map (fun (username, password, favPinnery) -> username, (username, password, favPinnery))
-            |> InMemoryDatabase.ofSeq
+    member val users: InMemoryDatabase<string, string * string * Option<string>> =
+        [ "sander", User.hashPassword "Password12", None
+          "jeroen", User.hashPassword "Password34", Some "Pingum"
+          "martijn", User.hashPassword "Password56", Some "LaBroche" ]
+        |> Seq.map (fun (username, password, favPinnery) -> username, (username, password, favPinnery))
+        |> InMemoryDatabase.ofSeq
