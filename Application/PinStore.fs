bc99c2cb542862960c51082347d66ea2aad9f59f
Sander Jochems <sanderjochems@hotmail.nl>
2022-06-21 16:00:55 +0200

    Create PinStore

---
+++ Application/PinStore.fs
@@ -0,0 +1,16 @@
+﻿namespace Pinfold.Application.PinStore
+
+open Pinfold.Model
+
+type IPinStore =
+    abstract all: seq<Pin>
+    
+    abstract wherePinnery: string -> seq<Pin>
+
+    abstract get: string -> Option<Pin>
+    
+    abstract getWherePinnery: string -> string -> Option<Pin>
+
+    abstract insert: Pinnery -> Pin -> bool
+
+    abstract update: Pinnery -> Pin -> Pin -> unit
---
+++ DataAccess/PinStore.fs
@@ -0,0 +1,39 @@
+﻿module Pinfold.DataAccess.PinStore
+
+open Pinfold.Application.PinStore
+open Pinfold.DataAccess.Database
+open Pinfold.DataAccess.Store
+open Pinfold.Model
+
+type PinStore(store: Store) =
+    let pinStore = store.pins
+
+    let mapPin ((name, value, _): string * decimal * string) : Pin = { Pin.Name = name; Value = value }
+
+    interface IPinStore with
+        member this.all =
+            InMemoryDatabase.all pinStore |> Seq.map mapPin
+
+        member this.wherePinnery(pinneryName: string) =
+            InMemoryDatabase.filter (fun (_, _, p) -> p = pinneryName) pinStore
+            |> Seq.map mapPin
+
+        member this.get(name: string) =
+            InMemoryDatabase.lookup name pinStore
+            |> Option.map mapPin
+
+        member this.getWherePinnery (pinneryName: string) (pinName: string) =
+            InMemoryDatabase.filter (fun (n, _, p) -> n = pinName && p = pinneryName) pinStore
+            |> Seq.tryHead
+            |> Option.map mapPin
+
+        member this.insert (pinnery: Pinnery) (pin: Pin) =
+            let result =
+                InMemoryDatabase.insert pin.Name (pinnery.Name, pin.Value, pinnery.Name) pinStore
+
+            match result with
+            | Ok _ -> true
+            | Error _ -> false
+
+        member this.update (pinnery: Pinnery) (pin: Pin) (newPin: Pin) =
+            InMemoryDatabase.update pin.Name (newPin.Name, newPin.Value, pinnery.Name) pinStore
